1d84557d47c92122863d6d4eaccb0a11ded85ef2
augustofrade <aquaaticnaotrava@gmail.com>
2025-09-09 22:27:20 -0300

    Minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Response } from "express";
 import { join } from "path";
 import YoutubeDownloaderService from "./core/services/youtube-downloader.service";
 import { extractVideoIdMiddleware } from "./middleware/extract-video-id.middleware";
@@ -6,27 +6,25 @@ import { extractVideoIdMiddleware } from "./middleware/extract-video-id.middlewa
 const app = express();
 app.use(express.json());
 
-const downloaderService = new YoutubeDownloaderService("tmp");
+const downloaderService = new YoutubeDownloaderService();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_, res: Response) => {
   const filePath = join(__dirname, "static", "index.html");
   res.sendFile(filePath);
 });
 
-app.get("/health", (req: Request, res: Response) => {
+app.get("/health", (_, res: Response) => {
   res.json({ status: "healthy" });
 });
 
-app.get("/video-details", extractVideoIdMiddleware, async (req: Request, res: Response) => {
+app.get("/video-details", extractVideoIdMiddleware, async (_, res: Response) => {
   const videoId = res.locals.videoId;
 
   const videoDetails = await downloaderService.getVideoDetails(videoId);
   return res.json({ data: videoDetails });
 });
 
-app.get("/download", extractVideoIdMiddleware, async (req: Request, res: Response) => {
-  console.log("Received");
-
+app.get("/download", extractVideoIdMiddleware, async (_, res: Response) => {
   const videoId = res.locals.videoId;
   const { readableStream, contentLength, filename } = await downloaderService.download(videoId);
 
