a6d6228afcab3ff1eb7ef25a1f517191ebee7bea
augustofrade <aquaaticnaotrava@gmail.com>
2025-09-10 11:16:43 -0300

    Fixes to sanitizeFilename function - Spaces from removed characters are now handled

--- src/core/helpers/sanitize-filename.ts
+++ src/core/helpers/sanitize-filename.ts
@@ -1,3 +1,6 @@
 export default function sanitizeFilename(filename: string): string {
-  return filename.replace(/[^a-zA-Z0-9\s-\[\]\(\)]/g, "");
+  return filename
+    .replace(/[^a-zA-Z0-9\s-\[\]\(\)]/g, "")
+    .replace(/\s+/g, " ")
+    .trim();
 }
