868c1bbcfba17fb02f9ba3747edcf70283ab88a7
augustofrade <aquaaticnaotrava@gmail.com>
2025-09-10 22:19:50 -0300

    Fixed thumbnail fetching

--- src/core/services/youtube-downloader.service.ts
+++ src/core/services/youtube-downloader.service.ts
@@ -1,4 +1,4 @@
-import ytdl from "@distube/ytdl-core";
+import ytdl, { thumbnail as YtdlThumbnail } from "@distube/ytdl-core";
 import { formatBytes } from "../helpers/format-bytes";
 import { formatDuration } from "../helpers/format-duration";
 import sanitizeFilename from "../helpers/sanitize-filename";
@@ -20,12 +20,14 @@ export default class YoutubeDownloaderService {
   }
 
   public async getVideoDetails(videoId: string): Promise<VideoDetails> {
+    const getLargestThumbnail = (thumbnails: YtdlThumbnail[]) => thumbnails[thumbnails.length - 1];
+
     const info = await ytdl.getInfo("https://youtube.com/watch?v=" + videoId);
     const videoFormat = ytdl.chooseFormat(info.formats, { filter: "audioonly" });
 
     return {
       title: info.videoDetails.title,
-      thumbnailUrl: info.thumbnail_url,
+      thumbnailUrl: getLargestThumbnail(info.videoDetails.thumbnails).url,
       filename: sanitizeFilename(info.videoDetails.title) + ".mp3",
       author: info.videoDetails.author.name,
       publishDate: info.videoDetails.publishDate,
